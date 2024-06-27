b22630bdee9b3ebaab5a799d576a513d1703cca4
Milton Mannarino <grountish@gmail.com>
2024-06-27 15:41:27 +0200

    Update event dates in index.html

---
+++ js/counter.js
@@ -0,0 +1,37 @@
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  // Define your countdown dates here
+  const countdownDates = ['11/30/2024', '12/06/2024', '12/06/2024'];
+
+  countdownDates.forEach((date, index) => {
+    const countDown = new Date(date).getTime(),
+      intervalId = setInterval(function () {
+        const now = new Date().getTime(),
+          distance = countDown - now;
+
+        document.getElementById('days' + (index + 1)).innerText =
+          Math.floor(distance / day);
+        document.getElementById('hours' + (index + 1)).innerText =
+          Math.floor((distance % day) / hour);
+        document.getElementById('minutes' + (index + 1)).innerText =
+          Math.floor((distance % hour) / minute);
+        document.getElementById('seconds' + (index + 1)).innerText =
+          Math.floor((distance % minute) / second);
+
+        // Do something later when the date is reached
+        if (distance < 0) {
+          document.getElementById(
+            'headline' + (index + 1)
+          ).innerText = 'Event has started!';
+          document.getElementById(
+            'countdown' + (index + 1)
+          ).style.display = 'none';
+          clearInterval(intervalId);
+        }
+      }, 1000);
+  });
+})();
