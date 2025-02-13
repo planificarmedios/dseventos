541401f92fc072e74d07e61a62db2fc639836cfc
Milton Mannarino <grountish@gmail.com>
2025-02-13 19:41:07 +0100

    2024 to 2025

--- js/counter.js
+++ js/counter.js
@@ -5,7 +5,7 @@
     day = hour * 24;
 
   // Define your countdown dates here
-  const countdownDates = ['11/30/2024', '12/06/2024', '12/06/2024'];
+  const countdownDates = ['11/30/2025', '12/06/2025', '12/06/2025'];
 
   countdownDates.forEach((date, index) => {
     const countDown = new Date(date).getTime(),
@@ -13,23 +13,21 @@
         const now = new Date().getTime(),
           distance = countDown - now;
 
-        document.getElementById('days' + (index + 1)).innerText =
-          Math.floor(distance / day);
-        document.getElementById('hours' + (index + 1)).innerText =
-          Math.floor((distance % day) / hour);
-        document.getElementById('minutes' + (index + 1)).innerText =
-          Math.floor((distance % hour) / minute);
-        document.getElementById('seconds' + (index + 1)).innerText =
-          Math.floor((distance % minute) / second);
+        document.getElementById('days' + (index + 1)).innerText = Math.floor(distance / day);
+        document.getElementById('hours' + (index + 1)).innerText = Math.floor(
+          (distance % day) / hour
+        );
+        document.getElementById('minutes' + (index + 1)).innerText = Math.floor(
+          (distance % hour) / minute
+        );
+        document.getElementById('seconds' + (index + 1)).innerText = Math.floor(
+          (distance % minute) / second
+        );
 
         // Do something later when the date is reached
         if (distance < 0) {
-          document.getElementById(
-            'headline' + (index + 1)
-          ).innerText = 'Event has started!';
-          document.getElementById(
-            'countdown' + (index + 1)
-          ).style.display = 'none';
+          document.getElementById('headline' + (index + 1)).innerText = 'Event has started!';
+          document.getElementById('countdown' + (index + 1)).style.display = 'none';
           clearInterval(intervalId);
         }
       }, 1000);
